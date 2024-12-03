a5b1108b38a073cb79961a118a863b2e365be54c
Mauro0407 <mauricio.mora.caicedo@udla.edu.ec>
2024-12-03 08:47:35 -0500

    Commit 1: Se creo todo lo relacionado con el stack layout

---
+++ MauricioMora-ExamenP2/Views/mmora_label1.xaml.cs
@@ -0,0 +1,47 @@
+namespace MauricioMora_ExamenP2.Views;
+
+public partial class mmora_label1 : ContentPage
+{
+	private string filepath;
+	public mmora_label1()
+	{
+		InitializeComponent();
+		BindingContext = this;
+		filepath = Path.Combine(FileSystem.AppDataDirectory, "MauricioMora.txt");
+		CargarUltimaRecarga();
+	}
+	public string UltimaRecarga {  get; set; }
+
+	private void CargarUltimaRecarga()
+	{
+		if (File.Exists(filepath))
+		{
+			UltimaRecarga = File.ReadAllText(filepath);
+		}
+		else
+		{
+			UltimaRecarga = "Sin recargas hechas previamente";
+		}
+		OnPropertyChanged(nameof(UltimaRecarga));
+	}
+	private async void OnRecargaClicked(object sender, EventArgs e)
+	{
+		string nombre = NameEntry.Text;
+		string telefono = PhoneEntry.Text;
+
+		if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(telefono))
+		{
+			await DisplayAlert("Error", "Debe llenar los campos", "Ok");
+			return;
+		}
+		string recargaInfo = $"Nombre : {nombre}, Teléfono: {telefono}, Fecha: {DateTime.Now}";
+		File.WriteAllText(filepath, recargaInfo);
+
+		await DisplayAlert("Exito", "La recarga fue exitosa", "Ok");
+
+		CargarUltimaRecarga();
+
+		NameEntry.Text = string.Empty;
+		PhoneEntry.Text = string.Empty;
+    }
+}
---
+++ MauricioMora-ExamenP2/Views/mmora_label2.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauricioMora_ExamenP2.Views;
+
+public partial class mmora_label2 : ContentPage
+{
+	public mmora_label2()
+	{
+		InitializeComponent();
+	}
+}
