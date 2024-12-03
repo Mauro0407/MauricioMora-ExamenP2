e34a09eafd71b613518bda200c45857b42d0e8d9
laboratorio <laboratorio@UPOAULA10302>
2024-12-03 07:39:30 -0500

    Agregar archivos de proyecto.

---
+++ MauricioMora-ExamenP2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauricioMora_ExamenP2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauricioMora-ExamenP2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauricioMora_ExamenP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
